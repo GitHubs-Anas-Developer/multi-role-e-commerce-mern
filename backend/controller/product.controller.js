849da344f9cf0b7c7ab80a8b1a7e318a81d69513
Anas Developer <anasfsdeveloper@gmail.com>
2026-03-09 20:52:54 +0400

    feat(product): add delete product API

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -170,3 +170,37 @@ export const getAllProducts = async (req, res) => {
     });
   }
 };
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const productId = req.params.id
+
+    console.log("productId",productId)
+
+    if (!productId)
+      return res.status(400).json({
+        success: false,
+        message: "Product id is required",
+      });
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+      deletedProduct,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import upload from "../middleware/multer.js";
 import { verifyAdmin } from "../middleware/auth.middleware.js";
-import { createProduct ,getAllProducts} from "../controller/product.controller.js";
+import { createProduct ,deleteProduct,getAllProducts} from "../controller/product.controller.js";
 const router = express.Router();
 
 router.post("/create", verifyAdmin, upload.array("images", 10), createProduct);
 router.post("/", verifyAdmin, getAllProducts);
+router.delete("/delete/:id", verifyAdmin, deleteProduct);
+
 
 export default router;
